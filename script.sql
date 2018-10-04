9b676bd917777f8faf4643b79e4e43e6d6b407ce
loov <loov1625@gmail.com>
2018-10-04 15:23:52 -0500

    Cree la tabla carreras

--- script.sql
+++ script.sql
@@ -1,2 +1,11 @@
 create database if not exist UNSIS;
 use UNSIS;
+
+create table Carreras{
+	idcarrera integer primary key,
+	nombre varchar(20),
+	carreras varchar(15),
+	jefecarrera varchar(20),
+	foreign key(nombreplan) references Plan_de_estudios(nombreplan);	
+}
+
