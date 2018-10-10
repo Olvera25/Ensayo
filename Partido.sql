c8d51c3f7fc858efd445f4a746fa17daee6b1b67
loov <loov1625@gmail.com>
2018-10-10 14:58:10 -0500

    script partido

--- Partido.sql
+++ Partido.sql
@@ -1,30 +1,33 @@
-create database if not exist PARTIDO;
+create database PARTIDO;
 use PARTIDO;
 
-create table Jugador{
+create table Jugador(
 	id_jugador integer primary key,
 	nombrej varchar(20),
 	apellidoj_p varchar(20),
 	apellidoj_m varchar(20),
 	telefonoj integer,
 	direccionj varchar(30),
-	puntosj integer;
-}
+	puntosj integer
+);
 
 
-create table Campeon{
+create table Campeon(
 	id_campeon integer primary key,
 	nombrec varchar(20),
 	apellidoc_p varchar(20),
 	apellidoc_m varchar(20),
 	telefonoc integer,
 	direccionc varchar(30),
-	puntosc integer;
-}
+	puntosc integer
+);
 
 
-create table Enfrenta{
+create table Enfrenta(
+	id_jugador integer,
+    id_campeon integer,
+	num_juegos integer,
 	foreign key(id_jugador) references Jugador(id_jugador),
-	foreign key(id_campeon) references Campeon(id_campeon),
-	num_juegos integer;
-}
+	foreign key(id_campeon) references Campeon(id_campeon)
+	
+);
